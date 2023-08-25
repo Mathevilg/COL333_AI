401a5ba75fc808f306e4945cf74817f454b8dab4
mani-sarthak <manisarthakiitd@gmail.com>
2023-08-25 12:32:50 +0530

    modified for actual testes

--- A1/starter_code/generateRandomTest.cpp
+++ A1/starter_code/generateRandomTest.cpp
@@ -6,8 +6,8 @@ using namespace std;
 #include <vector>
 #include <random>
 
-void gen(int matrixSize) {
-    std::vector<std::vector<int>> matrix(matrixSize, std::vector<int>(matrixSize, 0));
+void gen(int matrixSize, int state) {
+    std::vector<std::vector<int> > matrix(matrixSize, std::vector<int>(matrixSize, 0));
 
     // Seed the random number generator
     std::random_device rd;
@@ -15,12 +15,24 @@ void gen(int matrixSize) {
     std::uniform_int_distribution<int> distribution(1, 100); // Range for random integers
 
     // Fill the matrix with random values
-    for (int i = 0; i < matrixSize; ++i) {
-        for (int j = i+1; j < matrixSize; ++j) {
-            if (i != j) {
+    if (!state)
+    {
+        for (int i = 0; i < matrixSize; ++i) {
+            for (int j = 0; j < matrixSize; ++j) {
+                matrix[i][j] = distribution(generator);
+            }
+        }
+    }
+    else 
+    {
+        for (int i = 0; i < matrixSize; ++i) {
+            for (int j = i+1; j < matrixSize; ++j) {
+                
                 matrix[i][j] = distribution(generator);
                 matrix[j][i] = matrix[i][j];
+                
             }
+            matrix[i][i] = 0;
         }
     }
 
@@ -40,7 +52,7 @@ int main(){
     cout << t << endl;
     cout << z << endl;
     cout << l << endl;
-    gen(z);
-    gen(l);
+    gen(z, 0);
+    gen(l, 1);
     return 0;
 }
