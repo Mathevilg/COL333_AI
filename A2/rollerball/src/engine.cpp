4f555f9b65b77c5938a781f84e1e14c9ca94e599
ShlokDotPande <shlok.s.pande@gmail.com>
2023-09-21 19:29:35 +0530

    Added pawn score and check score
    weights 10, 5, 5

--- A2/rollerball/src/engine.cpp
+++ A2/rollerball/src/engine.cpp
@@ -9,6 +9,7 @@ int move_number = 0;
 using namespace std;
 
 # define MAX_DEPTH 3
+// actual depth is this value + 1. So if MAX_DEPTH = 3, then depth = 4
 
 int calculate_material(const Board& b)
 {
@@ -48,13 +49,100 @@ int calculate_material(const Board& b)
     return material;
 }
 
+int count_pawn_score(Board b)
+{
+    int pawn_score = 0;
+
+    pair<int, int> wp1 = make_pair(getx(b.data.w_pawn_ws), gety(b.data.w_pawn_ws));
+    pair<int, int> wp2 = make_pair(getx(b.data.w_pawn_bs), gety(b.data.w_pawn_bs));
+    pair<int, int> bp1 = make_pair(getx(b.data.b_pawn_ws), gety(b.data.b_pawn_ws));
+    pair<int, int> bp2 = make_pair(getx(b.data.b_pawn_bs), gety(b.data.b_pawn_bs));
+
+    if (wp1.first == 0 || wp1.first == 1)
+    {
+        if (wp1.second == 6)
+            pawn_score += 5 + wp1.first;
+        else
+            pawn_score += (wp1.second + 1);
+    }
+    else if (wp1.second == 5 || wp1.second == 6)
+    {
+        pawn_score += 5+wp1.first;
+    }
+
+    if (wp2.first == 0 || wp2.first == 1)
+    {
+        if (wp2.second == 6)
+            pawn_score += 5 + wp2.first;
+        else
+            pawn_score += (wp2.second + 1);
+    }
+    else if (wp2.second == 5 || wp2.second == 6)
+    {
+        pawn_score += 5+wp2.first;
+    }
+
+    if (bp1.first == 5 || bp1.first == 6)
+    {
+        if (bp1.second == 0)
+            pawn_score -= 5 + (6-bp1.first);
+        else
+            pawn_score -= (7-bp1.second);
+    }
+    else if (bp1.second == 1 || bp1.second == 0)
+    {
+        pawn_score -= 5+ (6-bp1.first);
+    }
+
+    if (bp2.first == 5 || bp2.first == 6)
+    {
+        if (bp2.second == 0)
+            pawn_score -= 5 + (6-bp2.first);
+        else
+            pawn_score -= (7-bp2.second);
+    }
+    else if (bp2.second == 1 || bp2.second == 0)
+    {
+        pawn_score -= 5+ (6-bp2.first);
+    }
+
+    return pawn_score;
+}
+
+
+int calc_check_score(Board b)
+{
+
+    if (b.in_check())
+    {
+        if (b.data.player_to_play == WHITE)
+            return -10;
+        else
+            return 10;
+    }
+    else
+        return 0;
+}
+
+
+int evaluate_function(Board b)
+{
+    int material = calculate_material(b);
+    int w1 = 10;
+    int pawn_score = count_pawn_score(b);
+    int w2 = 5;
+    int check_score = calc_check_score(b);
+    int w3 = 5;
+    return (w1*material + w2*pawn_score + w3*check_score);
+}
+
 pair<int, U16> Min_value(Board b, int depth, int alpha, int beta, Engine* e);
 
 pair<int, U16> Max_value(Board b, int depth, int alpha, int beta, Engine* e)
 {
     if (depth > MAX_DEPTH)
     {
-        return make_pair(calculate_material(b), 0);
+        return make_pair(evaluate_function(b), 0);
     }
     else
     {
@@ -91,7 +179,7 @@ pair<int, U16> Max_value(Board b, int depth, int alpha, int beta, Engine* e)
 pair<int, U16> Min_value(Board b, int depth, int alpha, int beta, Engine* e)
 {
     if (depth > MAX_DEPTH) {
-        return make_pair(calculate_material(b), 0);
+        return make_pair(evaluate_function(b), 0);
     }
     else {
         U16 best_move = 0;
